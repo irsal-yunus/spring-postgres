5c324fc85b7e61db8a83882107c48d287695c153
irsal yunus <irsal@code.id>
2021-02-11 11:32:04 +0700

    update script ci and tools service java

--- src/main/resources/spring-postgres.sh
+++ src/main/resources/spring-postgres.sh
@@ -11,13 +11,13 @@ f_pid=$PIDDIR/$APP_NAME.pid
 
 JAR_NAME=$APP_NAME.jar
 CONFIG_LOCATION=application.properties
-LOGGING_CONFIG=logback.xml
+
 
 export startDir=`pwd`
 
 cd $WORKINGDIR
 
-$java_bin -jar -server -Duser.timezone=GMT -Ddefault.config.dir=$startDir -Dfile.encoding=ISO8859_1 -Xms${java_min} -Xmx${java_max} -XX:MaxPermSize=${java_perm} $JAR_NAME --spring.config.location=$CONFIG_LOCATION --logging.config=$LOGGING_CONFIG > /dev/null &
+$java_bin -jar -server -Duser.timezone=GMT -Ddefault.config.dir=$startDir -Dfile.encoding=ISO8859_1 -Xms${java_min} -Xmx${java_max} -XX:MaxPermSize=${java_perm} $JAR_NAME --spring.config.location=$CONFIG_LOCATION  > /dev/null &
 
 PID=$!
 
